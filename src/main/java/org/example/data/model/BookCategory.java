e7f263ebacb65508c9d044a9a380276940112ac7
philip <ajibolaphilip10@gmail.com>
2024-04-30 01:06:23 +0100

    update

---
+++ src/main/java/org/example/data/model/BookCategory.java
@@ -0,0 +1,13 @@
+package org.example.data.model;
+
+import lombok.Data;
+
+
+public enum BookCategory {
+    MYSTERY,
+    SCIENCE_FRICTION,
+    HORROR,
+    HISTORICAL_FRICTION,
+    CHILDREN_BOOK,
+    POETRY
+}
