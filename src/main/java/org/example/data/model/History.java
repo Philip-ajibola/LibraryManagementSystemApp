5b37436d4b7bf79ac338534eaa2ecbec014a929b
philip <ajibolaphilip10@gmail.com>
2024-04-30 01:08:53 +0100

    update

--- src/main/java/org/example/data/model/History.java
+++ src/main/java/org/example/data/model/History.java
@@ -7,11 +7,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDate;
 @Data
 @Document("/transactions")
-public class Transaction {
+public class History {
     @Id
     private String id;
     private Book book;
-    private User user;
+    private String borrowerName;
     private BookStatus bookStatus;
     private LocalDate borrowedDate;
     private LocalDate returnDate;
