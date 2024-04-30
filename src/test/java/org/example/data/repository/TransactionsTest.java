87bd98b03697d5cec0de6841f12609ed09a9d0ae
philip <ajibolaphilip10@gmail.com>
2024-04-30 01:12:38 +0100

    update

--- src/test/java/org/example/data/repository/TransactionsTest.java
+++ src/test/java/org/example/data/repository/TransactionsTest.java
@@ -1,6 +1,6 @@
 package org.example.data.repository;
 
-import org.example.data.model.Transaction;
+import org.example.data.model.History;
 import org.example.data.model.BookStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +12,8 @@ public class TransactionsTest {
     private Transactions transactions;
     @Test
     public void testThatTransactionCanBeSaved(){
-        Transaction transaction = new Transaction();
-        transaction.setBookStatus(BookStatus.RETURNED);
-        transactions.save(transaction);
+        History history = new History();
+        history.setBookStatus(BookStatus.RETURNED);
+        transactions.save(history);
     }
 }
