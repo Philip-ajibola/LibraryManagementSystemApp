45b7acb943028842f9cd0b613147cde5320e6396
philip <ajibolaphilip10@gmail.com>
2024-04-30 03:44:58 +0100

    update

---
+++ src/main/java/org/example/exception/BookCategoryException.java
@@ -0,0 +1,7 @@
+package org.example.exception;
+
+public class BookCategoryException extends LibraryManagementSystemException{
+    public BookCategoryException(String message) {
+        super(message);
+    }
+}
