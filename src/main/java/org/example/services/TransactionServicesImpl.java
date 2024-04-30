d82a690cc10f0019282957cba0d9371952f18744
philip <ajibolaphilip10@gmail.com>
2024-04-30 01:12:21 +0100

    update

--- src/main/java/org/example/services/TransactionServicesImpl.java
+++ src/main/java/org/example/services/TransactionServicesImpl.java
@@ -1,30 +1,33 @@
 package org.example.services;
 
 import org.example.data.model.Book;
-import org.example.data.model.BookStatus;
-import org.example.data.model.Transaction;
-import org.example.data.model.User;
+import org.example.data.model.History;
+import org.example.data.repository.Books;
 import org.example.data.repository.Transactions;
+import org.example.exception.NoBookHistoryFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TransactionServicesImpl implements TransactionServices{
     @Autowired
     private Transactions transactions;
+    @Autowired
+    private Books books;
 
     @Override
-    public void save(Transaction transaction) {
-        transactions.save(transaction);
+    public void save(History history) {
+        transactions.save(history);
     }
 
     @Override
-    public List<Transaction> findAll() {
+    public List<History> findAll() {
         return transactions.findAll();
     }
+
 
 
 }
