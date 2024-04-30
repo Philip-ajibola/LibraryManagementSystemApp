04af7408f730fc5d744cfd5acd33002bd529f532
philip <ajibolaphilip10@gmail.com>
2024-04-30 03:45:20 +0100

    update

--- src/main/java/org/example/data/repository/Books.java
+++ src/main/java/org/example/data/repository/Books.java
@@ -1,10 +1,16 @@
 package org.example.data.repository;
 
 import org.example.data.model.Book;
+import org.example.data.model.BookCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 public interface Books extends MongoRepository<Book,String> {
     void deleteByIsbn(String isbn);
 
     Book findByIsbn(String isbn);
+
+    List<Book> findByBookCategory(BookCategory category);
+    List<Book> findBookByAuthorName(String authorName);
 }
