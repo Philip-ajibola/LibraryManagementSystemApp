d86f596988cd9ab4317725b0206cdafe9271ffa8
philip <ajibolaphilip10@gmail.com>
2024-04-30 01:11:39 +0100

    update

--- src/main/java/org/example/data/repository/Transactions.java
+++ src/main/java/org/example/data/repository/Transactions.java
@@ -1,10 +1,11 @@
 package org.example.data.repository;
 
 import org.example.data.model.Book;
-import org.example.data.model.Transaction;
+import org.example.data.model.History;
 import org.example.data.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface Transactions extends MongoRepository<Transaction,String> {
-    Transaction findByUserAndBook(User user, Book book);
+import java.util.List;
+
+public interface Transactions extends MongoRepository<History,String> {
 }
