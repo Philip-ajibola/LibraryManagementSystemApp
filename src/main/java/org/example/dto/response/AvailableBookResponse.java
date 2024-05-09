9de944498568fd12ec6d690dab7bd63ff469522b
philip <ajibolaphilip10@gmail.com>
2024-05-09 01:48:15 +0100

    an update was made

--- src/main/java/org/example/dto/response/AvailableBookResponse.java
+++ src/main/java/org/example/dto/response/AvailableBookResponse.java
@@ -1,11 +1,13 @@
 package org.example.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import org.example.data.model.BookCategory;
 
 import java.time.LocalDate;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AvailableBookResponse {
     private String id;
     private String author;
