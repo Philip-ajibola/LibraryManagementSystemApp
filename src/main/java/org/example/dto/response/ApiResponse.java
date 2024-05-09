37c1e8c682f633156c28df820696e57a8a4c3364
philip <ajibolaphilip10@gmail.com>
2024-05-09 01:47:53 +0100

    an update was made

--- src/main/java/org/example/dto/response/ApiResponse.java
+++ src/main/java/org/example/dto/response/ApiResponse.java
@@ -1,10 +1,12 @@
 package org.example.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiResponse {
     private boolean isSuccessful;
     private Object object;
