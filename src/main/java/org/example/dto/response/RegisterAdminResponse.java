006fa402938a11818c94f6e37cd64193b53fd536
philip <ajibolaphilip10@gmail.com>
2024-05-09 01:51:58 +0100

    an update was made

--- src/main/java/org/example/dto/response/RegisterAdminResponse.java
+++ src/main/java/org/example/dto/response/RegisterAdminResponse.java
@@ -1,8 +1,10 @@
 package org.example.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RegisterAdminResponse {
     private String username;
     private String Id;
