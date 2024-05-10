a03d1bfb2d99e8e5cd8432ac81890a1a44cbdced
philip <ajibolaphilip10@gmail.com>
2024-05-10 13:35:52 +0100

    an update was made

--- src/main/java/org/example/dto/response/BorrowBookResponseForUser.java
+++ src/main/java/org/example/dto/response/BorrowBookResponseForUser.java
@@ -2,6 +2,7 @@ package org.example.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import org.example.data.model.BookCategory;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -9,5 +10,5 @@ public class BorrowBookResponseForUser {
     private String bookId;
     private String bookTitle;
     private String bookAuthor;
-    private String Category;
+    private BookCategory Category;
 }
