b43286fb8545e772182155c9d646152d97e9a5cb
philip <ajibolaphilip10@gmail.com>
2024-05-17 11:27:47 +0100

    an update was made

--- src/main/java/org/example/data/repository/Users.java
+++ src/main/java/org/example/data/repository/Users.java
@@ -6,4 +6,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface Users extends MongoRepository<User,String> {
 
     User findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
 }
